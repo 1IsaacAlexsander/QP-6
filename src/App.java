49921838046196a6d7af050e00c0560a94338452
Isaac Alexsander <126776974+1IsaacAlexsander@users.noreply.github.com>
2023-03-30 09:04:42 -0300

    Add files via upload

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int num;
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("Digite um número:");
+            num = sc.nextInt();
+            System.out.println("O número é: " + num);
+        } catch (Exception e) {
+            System.out.println("Caracter invalido.");
+            System.exit(0);
+        }
     }
 }
