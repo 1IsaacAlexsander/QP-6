b9e7dae2c228077f6a6deb6515fee3966b9b1b92
Isaac Alexsander <126776974+1IsaacAlexsander@users.noreply.github.com>
2023-03-30 09:14:58 -0300

    Add files via upload

--- src/App.java
+++ src/App.java
@@ -2,17 +2,28 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        int num;
-
         Scanner sc = new Scanner(System.in);
-
-        try {
+        int num = Integer.MIN_VALUE;
+        while (true) {
             System.out.println("Digite um número:");
-            num = sc.nextInt();
-            System.out.println("O número é: " + num);
-        } catch (Exception e) {
-            System.out.println("Caracter invalido.");
-            System.exit(0);
+            while (true) { // enquanto não digitar um número válido, continua no while
+                try {
+                    num = Integer.parseInt(sc.nextLine());
+                    System.out.println("O número digitado é: "+ num);
+                    break; // se chegou aqui é porque o número é válido, então posso sair do while
+                } catch (NumberFormatException e) {
+                    // número inválido
+                    System.out.println("Digite um número válido");
+                }
+            }
+        
+            // se for -1, sai do while mais externo
+            if (num != Integer.MIN_VALUE){
+                break;
+
+            }
+        
+           
         }
     }
 }
